164a1f989831edcfdd146bcb6050292bddd4aa21
gmanka <gmankab@gmail.com>
2022-07-05 15:39:37 +0400

    aboba

--- links_updater.py
+++ links_updater.py
@@ -11,19 +11,57 @@ from rich import pretty
 from rich.console import Console
 
 pretty.install()
-
 console = Console()
-
 print = console.print
+run = subprocess.getstatusoutput
 
 tweaker_dir = os.getcwd()
+
 gmankab_github_io_dir = Path(
     f'{tweaker_dir}/../gmankab.github.io'
 ).resolve()
 
-output = subprocess.getstatusoutput(
+tweaker_github_io_dir = f'{gmankab_github_io_dir}/arch'
+backup_dir = f'{gmankab_github_io_dir}/backup'
+
+files_list = run(
     'git ls-tree -r main --name-only'
-)[1]
+)[1].split('\n')
 
-print(output.split('\n'))
+ban_list = [
+    'readme.md',
+    '.gitignore',
+    'start.sh',
+    'links_updater.py',
+    'links_updater.sh',
+]
 
+for file in ban_list:
+    while file in files_list:
+        files_list.remove(file)
+
+for file in files_list.copy():
+    if file[:4] == 'svg/':
+        files_list.remove(file)
+
+print(run(f'rm -r {backup_dir}'))
+os.rename(tweaker_github_io_dir, backup_dir)
+
+for filename in files_list:
+    file = f'{tweaker_github_io_dir}/{filename}'
+    Path(file).parent.mkdir(
+        exist_ok=True,
+        parents=True,
+    )
+
+    file_content = f'<meta http-equiv="refresh" content="1;url=https://raw.githubusercontent.com/gmankab/gmankab.github.io/main/{filename}"/>'
+    command = f"echo '{file_content}' > '{file}'"
+    print(
+        run(
+            command
+        )
+    )
+
+os.system('python ~/proj/init/python/gp.py main')
+os.system(f'cd {gmankab_github_io_dir}')
+os.system('python ~/proj/init/python/gp.py main')
